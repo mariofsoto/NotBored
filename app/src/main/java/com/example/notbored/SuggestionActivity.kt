c4a6bb2de192fb22eb7357773f7825501a0961bd
ccisnero <camila.cisnero@mercadolibre.com>
2022-09-06 16:15:40 -0300

    Created suggestion activity

---
+++ app/src/main/java/com/example/notbored/SuggestionActivity.kt
@@ -0,0 +1,11 @@
+package com.example.notbored
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class SuggestionActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_suggestion)
+    }
+}
