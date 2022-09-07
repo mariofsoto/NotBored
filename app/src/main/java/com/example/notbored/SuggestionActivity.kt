80fb7d736628b926b7cb04bd5e2ebee4749e1c46
ccisnero <camila.cisnero@mercadolibre.com>
2022-09-07 10:28:26 -0300

    Added toolbar and icons

--- app/src/main/java/com/example/notbored/SuggestionActivity.kt
+++ app/src/main/java/com/example/notbored/SuggestionActivity.kt
@@ -2,10 +2,25 @@ package com.example.notbored
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import com.example.notbored.databinding.ActivitySuggestionBinding
 
 class SuggestionActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivitySuggestionBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_suggestion)
+
+        binding = ActivitySuggestionBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.topAppBar.setNavigationOnClickListener {
+            Toast.makeText(this, "HOLAA", Toast.LENGTH_SHORT).show()
+        }
+
+        binding.topAppBar.title = "Hola"
+
     }
+
 }
