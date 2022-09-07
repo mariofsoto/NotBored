e557eca63a41345bad63e41ba7f45267ec8f6e10
Mario Felipe Soto Zambrano <mario.soto@mercadolibre.com.co>
2022-09-07 10:38:28 -0500

    add the activities screen

---
+++ app/src/main/java/com/example/notbored/ScreenActivities.kt
@@ -0,0 +1,27 @@
+package com.example.notbored
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.ArrayAdapter
+import android.widget.ListView
+import android.widget.Toast
+
+class ScreenActivities : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activities_screen)
+
+        val arrayAdapter: ArrayAdapter<*>
+
+        val actividades = mutableListOf("Education","Recreational","Social","Diy","Charity","Cooking","Relaxation","Music","Busywork")
+        val lvDatos = findViewById<ListView>(R.id.lvActividades)
+
+        supportActionBar?.hide()
+
+        arrayAdapter = ArrayAdapter(this, android.R.layout.simple_list_item_1, actividades)
+        lvDatos.adapter = arrayAdapter
+        lvDatos.setOnItemClickListener(){parent, view, position, id ->
+            Toast.makeText(this, parent.getItemAtPosition(position).toString(), Toast.LENGTH_SHORT).show()
+        }
+    }
+}
