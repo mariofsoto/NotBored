d58ca4c3b7e00b8af76f6a725804808c1daf6e81
Mario Felipe Soto Zambrano <mario.soto@mercadolibre.com.co>
2022-09-09 09:47:05 -0500

    Warning were removed

--- app/src/main/java/com/example/notbored/MyListAdapter.kt
+++ app/src/main/java/com/example/notbored/MyListAdapter.kt
@@ -1,13 +1,16 @@
+package com.example.notbored
+
 import android.app.Activity
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ArrayAdapter
 import android.widget.TextView
-import com.example.notbored.R
+import android.annotation.SuppressLint
 
-class MyListAdapter(private val context: Activity, private val title: Array<String>)
-    : ArrayAdapter<String>(context, R.layout.custom_list, title) {
+class MyListAdapter(private val context: Activity, private val title: Array<String>) :
+    ArrayAdapter<String>(context, R.layout.custom_list, title) {
 
+    @SuppressLint("InflateParams", "ViewHolder")
     override fun getView(position: Int, view: View?, parent: ViewGroup): View {
         val inflater = context.layoutInflater
         val rowView = inflater.inflate(R.layout.custom_list, null, true)
--- app/src/main/java/com/example/notbored/ScreenActivities.kt
+++ app/src/main/java/com/example/notbored/ScreenActivities.kt
@@ -1,15 +1,12 @@
 package com.example.notbored
 
-import MyListAdapter
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.ArrayAdapter
-import android.widget.ListView
-import android.widget.Toast
 import com.example.notbored.databinding.ActivitiesScreenBinding
 
 class ScreenActivities : AppCompatActivity() {
+
     private lateinit var binding: ActivitiesScreenBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -17,39 +14,45 @@ class ScreenActivities : AppCompatActivity() {
         binding = ActivitiesScreenBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        val activities = arrayOf(
+            "Education",
+            "Recreational",
+            "Social",
+            "Diy",
+            "Charity",
+            "Cooking",
+            "Relaxation",
+            "Music",
+            "Busywork"
+        )
 
-        val actividades = arrayOf<String>("Education","Recreational","Social","Diy","Charity","Cooking","Relaxation","Music","Busywork")
-
-        val arrayAdapter = MyListAdapter(this,actividades)
-
-        //arrayAdapter = ArrayAdapter(this, android.R.layout.simple_list_item_1, actividades)
+        val arrayAdapter = MyListAdapter(this, activities)
 
         binding.lvActivities.adapter = arrayAdapter
-        binding.lvActivities.setOnItemClickListener(){parent, _, position, _ ->
-            Toast.makeText(this, parent.getItemAtPosition(position).toString(), Toast.LENGTH_SHORT).show()
 
-
-            goToActivity(SuggestionActivity::class.java, parent.getItemAtPosition(position).toString())
+        binding.lvActivities.setOnItemClickListener { parent, _, position, _ ->
+            goToActivity(
+                SuggestionActivity::class.java,
+                parent.getItemAtPosition(position).toString()
+            )
         }
-        binding.topAppBarActivities.setOnMenuItemClickListener {menuItem ->
+
+        binding.topAppBarActivities.setOnMenuItemClickListener { menuItem ->
             when (menuItem.itemId) {
                 R.id.randomCategory -> {
-                    goToActivity(SuggestionActivity::class.java,"random")
+                    goToActivity(SuggestionActivity::class.java, "random")
                     true
-
                 }
                 else -> false
             }
         }
 
-
-
-
-
     }
-    private fun goToActivity(cls : Class<*>, type : String){
-        val goToActivity = Intent(this,cls)
-            .putExtra("type",type)
+
+    private fun goToActivity(cls: Class<*>, type: String) {
+        val goToActivity = Intent(this, cls)
+            .putExtra("type", type)
         startActivity(goToActivity)
     }
+
 }
