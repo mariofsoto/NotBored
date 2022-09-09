7579a222d385cb6db18693100e19e02e514961f8
Mario Felipe Soto Zambrano <mario.soto@mercadolibre.com.co>
2022-09-09 08:00:11 -0500

    Configured MyListAdapter with the custom_list and set the communication with the ListView

---
+++ app/src/main/java/com/example/notbored/MyListAdapter.kt
@@ -0,0 +1,21 @@
+import android.app.Activity
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ArrayAdapter
+import android.widget.TextView
+import com.example.notbored.R
+
+class MyListAdapter(private val context: Activity, private val title: Array<String>)
+    : ArrayAdapter<String>(context, R.layout.custom_list, title) {
+
+    override fun getView(position: Int, view: View?, parent: ViewGroup): View {
+        val inflater = context.layoutInflater
+        val rowView = inflater.inflate(R.layout.custom_list, null, true)
+
+        val titleText = rowView.findViewById(R.id.title) as TextView
+
+        titleText.text = title[position]
+
+        return rowView
+    }
+}
